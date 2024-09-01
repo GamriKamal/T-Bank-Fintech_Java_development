7ce2ed62cebe1625c9a3481a0f00a289727a3334
GamriKamal <124342926+GamriKamal@users.noreply.github.com>
2024-09-01 18:14:52 +0500

    Delete T-Bank_parser directory

--- T-Bank_parser/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-
-public class Main {
-    public static void main(String[] args) {
-        ParserInfo parserInfo = new ParserInfo();
-        parserInfo.parse("src/main/resources/city.json", "src/main/resources/city_info.xml");
-    }
-}
