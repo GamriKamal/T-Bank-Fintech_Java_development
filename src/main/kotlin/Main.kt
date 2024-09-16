898feb3caf601f9487e56ffedc2fb84adb6f3dd3
GamriKamal <124342926+GamriKamal@users.noreply.github.com>
2024-09-16 21:04:19 +0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,8 @@ package org.example
 suspend fun main() {
     val result: List<News> = ApiKudaGo().getNews(10)
 
-    val startDate = ApiKudaGo().parseDate("13.09.2024")
-    val endDate = ApiKudaGo().parseDate("14.09.2024")
+    val startDate = ApiKudaGo().parseDate("15.09.2024")
+    val endDate = ApiKudaGo().parseDate("16.09.2024")
 
     val period = startDate..endDate
     val mostRatedNews = result.getMostRatedNews(10, period)
